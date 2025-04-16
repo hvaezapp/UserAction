1e8c04bcbaa3a69c3de0ea25f225a2fae63778ba
hassa <hassa@LAPTOP-U03CN6LR>
2025-04-16 16:12:31 +0330

    add broker config

--- src/UserAction/Registeration/RegisterDependency.cs
+++ src/UserAction/Registeration/RegisterDependency.cs
@@ -1,5 +1,6 @@
 ﻿using UserAction.DbContexts.Sql.SqlServer;
 using Microsoft.EntityFrameworkCore;
+using MassTransit;
 
 namespace UserAction.Registeration
 {
@@ -14,5 +15,38 @@ namespace UserAction.Registeration
             }, ServiceLifetime.Scoped);
         }
 
+
+        public static IServiceCollection RegisterBroker(this IServiceCollection services, IConfiguration configuration)
+        {
+            var host = configuration.GetValue<string>("BrokerConfiguration:Host") ?? throw new ArgumentNullException("hostname must valid");
+            var username = configuration.GetValue<string>("BrokerConfiguration:UserName") ?? throw new ArgumentNullException("username must valid");
+            var password = configuration.GetValue<string>("BrokerConfiguration:Password") ?? throw new ArgumentNullException("password must valid");
+
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(typeof(Program).Assembly);
+
+                configure.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.UseRawJsonDeserializer();
+
+                    cfg.Host(host, hostConfigure =>
+                    {
+                        hostConfigure.Username(username);
+                        hostConfigure.Password(password);
+                    });
+
+                    cfg.ConfigureEndpoints(context);
+                });
+
+            });
+
+            return services;
+        }
+
     }
+
+
+
+
 }
